a940d0eb622f90bdd00d649fec7138181fca893f
RozWhite <77862612+RozWhite@users.noreply.github.com>
2021-04-04 19:47:46 -0400

    your text

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+
+module.exports =
+{
+    //"URI": "mongodb://localhost/surveys"
+    "URI": "mongodb+srv://aleksandr:PyP64Lt4WX3u0xw9@cluster0.kfque.mongodb.net/surveys_db?retryWrites=true&w=majority"
+    //"URI": "mongodb+srv://blackhole:HoEr7tRqgcoHRLP9@cluster0.xyrc1.mongodb.net/surveys?retryWrites=true&w=majority"
+}
+    
